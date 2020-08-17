989ea47ce5c65f1dc84a7d559bf25dde78805e4a
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-08-17 15:25:59 +0200

    add header

--- src/pages/test.js
+++ src/pages/test.js
@@ -6,16 +6,12 @@ import { Aperture } from '../components/Aperture'
 import Test  from '../images/test.svg'
 import Logo from '../images/elitizon.svg'
 
+import Header from "../components/headers/light"
+
 const Square = tw.div`bg-pink-500 w-10 h-10`
 
 export default () => (
   <div>
-    <Square />
-    <Square tw="m-5 bg-green-500"></Square>
-    <div tw="bg-yellow-200 h-20 flex flex-col items-center justify-center">
-      <Aperture tw="fill-current text-pink-500 h-24"  />
-    </div>
-    <Test tw="text-pink-500 fill-current w-10 h-10"/>
-    <Logo tw="text-pink-500 fill-current w-1/2"/>
+    <Header/>
   </div>
 )
