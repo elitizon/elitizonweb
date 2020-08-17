1b622b8e6269b46896a6e4ada0e81a4b5297bbeb
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-08-17 12:11:17 +0200

    fix footer

--- src/components/footers/MiniCenteredFooter.js
+++ src/components/footers/MiniCenteredFooter.js
@@ -53,7 +53,7 @@ export default () => {
             </SocialLink>
           </SocialLinksContainer>
           <CopyrightText>
-            &copy; Copyright 2020, Treact Inc. All Rights Reserved.
+            &copy; Copyright 2020, elitizon ltd All Rights Reserved.
           </CopyrightText>
         </Row>
       </Content>
