0aeafd6523defe4b232a2f0160023a674a55634e
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-08-17 15:41:31 +0200

    add hero bacground image

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,17 +1,13 @@
 import React from "react"
 import tw from 'twin.macro'
 
-import { Aperture } from '../components/Aperture'
-
-import Test  from '../images/test.svg'
-import Logo from '../images/elitizon.svg'
 
 import Header from "../components/headers/light"
+import Hero from "../components/hero/BackgroundAsImage"
 
-const Square = tw.div`bg-pink-500 w-10 h-10`
 
 export default () => (
   <div>
-    <Header/>
+    <Hero/>
   </div>
 )
