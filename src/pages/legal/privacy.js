34820eaaebd85eee08649290307fc37357150ae5
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-08-28 19:02:56 +0800

    fix issue with HtmlContainer

--- src/pages/legal/privacy.js
+++ src/pages/legal/privacy.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Page } from "../../components/Page"
-import { Container } from "./HtmlContainer"
+import { Container } from "../../components/HtmlContainer"
 
 export default () => {
   return (
