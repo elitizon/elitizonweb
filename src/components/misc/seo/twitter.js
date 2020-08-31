845da50721f3c2f7f04fc3045aa4783c42ed1a0b
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-08-31 09:30:18 +0800

    remove twitter metadata already covered by og metadata

--- src/components/misc/seo/twitter.js
+++ src/components/misc/seo/twitter.js
@@ -11,13 +11,7 @@ export const Twitter = ({
 }) => (
   <Helmet>
     <meta name="twitter:card" content={type} />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={desc} />
     <meta name="twitter:image" content={image} />
-    {image && image.indexOf("https" > -1) && (
-      <meta name="twitter:image:secure_url" content={image} />
-    )}
-    <meta name="twitter:image:alt" content={desc} />
     <meta name="twitter:site" content={site} />
     {username && <meta name="twitter:creator" content={username} />}
   </Helmet>
