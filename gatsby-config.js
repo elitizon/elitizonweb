fd8a2bc09958510fd5bd480a1eab3c0d9740237a
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-09-06 23:08:12 +0800

    change encoding in RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -213,7 +213,8 @@ module.exports = {
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.rawBody }],
+                 // custom_elements: [{ "content:encoded": `${edge.node.rawBody}` }],
+                  custom_elements: [{ "content:unencoded": `${edge.node.rawBody}` }],
                 })
               })
             },
