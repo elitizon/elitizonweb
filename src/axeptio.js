8f9e10c131750dae299251587ae294b9b85598e3
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-04-13 10:14:34 +0800

    add cookies

---
+++ src/axeptio.js
@@ -0,0 +1,10 @@
+window.axeptioSettings = {
+  clientId: "6437627266c987ad7f30b56b",
+  cookiesVersion: "elitizon-en",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
